42b276359925dce214c2064cbab422bf47dc0c37
romita95 <84144627+romita95@users.noreply.github.com>
2024-03-26 19:29:56 +0100

    Modified image resolution

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const apiKey = "06c970a79fa97220038e7592a6045834";
-const apiUrl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=Maastricht";
+const apiUrl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=Bari";
 const image = document.querySelector("welcomeImage");
 
 async function checkWeather(){
@@ -10,16 +10,18 @@ async function checkWeather(){
     var data = await response.json();
 
     if(data.weather[0].main == "Drizzle"){
-        image.src = "images/rainyWeather.webp";
+        image.src = "images/rainyWeather.jpg";
     }else if(data.weather[0].main == "Clear"){
         image.src = "images/sunnyWeather.png";
     }else if(data.weather[0].main == "Clouds"){
         image.src = "images/cloudyWeather.png";
     }else if(data.weather[0].main == "Rainy"){
-        image.src = "images/rainyWeather.webp";
+        image.src = "images/rainyWeather.jpg";
+    }else if(data.weather[0].main == "Snow"){
+        image.src = "images/snowyWeather.jpg";
     }
 
-    
+    console.log(data);
 }
 
 checkWeather();
